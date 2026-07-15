1756e142ec7be451f7e6858cd2eef48f8dc7fd7f
akashselvins <akashselvins@gmail.com>
2026-07-15 18:14:15 +0530

    dsv

--- blocks/texttab/texttab.js
+++ blocks/texttab/texttab.js
@@ -41,6 +41,15 @@ export default function decorate(block) {
   if (wrapper) {
     wrapper.classList.add('active');
 
+    if (window.innerWidth <= 768) {
+  requestAnimationFrame(() => {
+    wrapper.parentElement.scrollIntoView({
+      behavior: 'auto',
+      inline: 'nearest',
+      block: 'nearest',
+    });
+  });
+}
   }
 
   }
